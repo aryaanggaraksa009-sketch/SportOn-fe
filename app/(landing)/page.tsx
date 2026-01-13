08ab74afe0138433b04bc483a973f8a8f8cd12de
Raksa <aryaanggaraksa009@gmail.com>
2026-01-13 22:51:45 +0700

    commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return ( <main className="bg-primary">
+    <h1 className="font-extrabold">Welcome to SportOn</h1>
+  </main>);
+}
