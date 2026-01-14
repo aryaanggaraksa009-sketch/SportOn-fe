164909819ace8da556cfbc9465422f875d7f2461
Raksa <aryaanggaraksa009@gmail.com>
2026-01-14 17:18:41 +0700

    lates update

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "../globals.css";
+import Header from "./components/layouts/header";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -23,8 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased`}
-      >
+        className={`${poppins.variable} antialiased`}>
+          <Header />
         {children}
       </body>
     </html>
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import { TbShoppingBagExclamation } from "react-icons/tb";
 
 export default function Home() {
   return ( <main className="bg-primary">
     <h1 className="font-extrabold">Welcome to SportOn</h1>
   </main>);
+  <TbShoppingBagExclamation />
 }
