f23ff4de066d54ac4798a6b763e8710e87ee5849
Raksa <aryaanggaraksa009@gmail.com>
2026-01-18 15:56:17 +0700

    lates update

--- app/(landing)/components/home/categories.tsx
+++ app/(landing)/components/home/categories.tsx
@@ -27,27 +27,27 @@ const categoryList = [
         name: "Swimming",
         imgUrl: "category-swimming.png",
     },
-]
+];
 
 const CategoriesSection = () => {
     return (
-        <section id="categories-section" className="container mx-auto">
+        <section id="category-section" className="container mx-auto pb-20">
             <div className="flex justify-between">
                 <h2 className="font-bold text-2xl">Browse By Categories</h2>
                 <Link href="#" className="flex gap-2 text-primary font-medium"><span className="self-center">See All Categories </span><FiArrowRight className="self-center"/></Link>
             </div>
             <div className="grid grid-cols-6 gap-12 mt-8">
                 {categoryList.map((category, index) => (
-                    <div className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7D1] w-full aspect-square flex justify-center" key={index}>
+                    <div className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7D1] w-full aspect-square flex flex-col item-center justify-center" key={index}>
                         <div className="self-center">
-                             <Image src={`/images/categories/${category.imgUrl}`} width={86} height="86" alt={category.name} className="mb-[10px]"/>
+                             <Image src={`/images/categories/${category.imgUrl}`} width={86} height={86} alt={category.name} className="mb-2"/>
                         </div>
                     <div className="text-primary font-medium text-xl text-center">{category.name}</div>
                 </div>   
                 ))}
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default CategoriesSection;
